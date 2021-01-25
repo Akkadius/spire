63e1cedc8ab813f0da6e123be0e65d4fa10c5aeb
Akkadius <akkadius1@gmail.com>
2021-01-24 22:52:13 -0600

    Update app.ts

--- frontend/src/constants/app.ts
+++ frontend/src/constants/app.ts
@@ -1,6 +1,6 @@
 // export const ASSET_CDN_BASE_URL = "https://rawcdn.githack.com/Akkadius/eq-asset-preview/36903141f431d94766eb83782e9445a856f23514/"
 
-const CDN_VERSION_HASH       = '0b9ad41aae3fda364745ebae5cb55565c7ce732d'
+const CDN_VERSION_HASH = '0b9ad41aae3fda364745ebae5cb55565c7ce732d'
 
 // List of different git CDN's
 // const ASSET_CDN_BASE_URL_INT = `https://gitcdn.xyz/repo/Akkadius/eq-asset-preview/${CDN_VERSION_HASH}/`
@@ -15,6 +15,6 @@ export const App = {
   ASSET_OBJECTS_BASE_URL: ASSET_CDN_BASE_URL_INT + 'assets/objects/',
   ASSET_SPELL_ICONS_BASE_URL: ASSET_CDN_BASE_URL_INT + 'assets/spell_icons/',
   ASSET_SPELL_ANIMATIONS: ASSET_CDN_BASE_URL_INT + 'assets/spell_animations/',
-  BACKEND_BASE_URL: process.env.VUE_APP_BACKEND_BASE_URL,
+  BACKEND_BASE_URL: (process.env.VUE_APP_BACKEND_BASE_URL ? process.env.VUE_APP_BACKEND_BASE_URL : window.location.host),
   DEBUG: false,
 }
