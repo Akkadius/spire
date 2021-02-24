de7ede225b013800c636d75523509e52e299cd73
Akkadius <akkadius1@gmail.com>
2021-02-23 23:39:10 -0600

    Add hook for source examples updating

--- http/controllers/quest_api_controller.go
+++ http/controllers/quest_api_controller.go
@@ -25,7 +25,8 @@ func NewQuestApiController(
 func (d *QuestApiController) Routes() []*routes.Route {
 	return []*routes.Route{
 		routes.RegisterRoute(http.MethodGet, "quest-api/definitions", d.getQuestDefinitions, nil),
-		routes.RegisterRoute(http.MethodPost, "quest-api/webhook-update-api", d.webhookUpdateApi, nil),
+		routes.RegisterRoute(http.MethodPost, "quest-api/webhook-update-api", d.webhookSourceDefinitionsUpdateApi, nil),
+		routes.RegisterRoute(http.MethodPost, "quest-api/webhook-update-source-examples/org/:org/repo/:repo/branch/:branch", d.webhookSourceExamplesUpdateApi, nil),
 		routes.RegisterRoute(
 			http.MethodPost,
 			"quest-api/source-examples/org/:org/repo/:repo/branch/:branch",
@@ -67,11 +68,27 @@ func (d *QuestApiController) searchGithubExamples(c echo.Context) error {
 }
 
 // ingests a webhook from Github and updates the repo data locally
-func (d *QuestApiController) webhookUpdateApi(c echo.Context) error {
+func (d *QuestApiController) webhookSourceDefinitionsUpdateApi(c echo.Context) error {
 	// todo: verify signature later
 	if c.Request().Header.Get("X-Github-Event") != "" &&
 		c.Request().Header.Get("X-Github-Delivery") != "" {
 		d.parser.Parse(true)
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{"data": "Ok"})
+}
+
+// ingests a webhook from Github and updates the repo data locally
+func (d *QuestApiController) webhookSourceExamplesUpdateApi(c echo.Context) error {
+	// todo: verify signature later
+	if c.Request().Header.Get("X-Github-Event") != "" &&
+		c.Request().Header.Get("X-Github-Delivery") != "" {
+		// params
+		org := c.Param("org")
+		repo := c.Param("repo")
+		branch := c.Param("branch")
+
+		d.sourcer.Source(org, repo, branch, true);
 	}
 
 	return c.JSON(http.StatusOK, echo.Map{"data": "Ok"})
