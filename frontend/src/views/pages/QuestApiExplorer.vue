1b9aa5c0d9e95a9f4e1b837302843757e02153d4
Akkadius <akkadius1@gmail.com>
2021-03-25 23:47:14 -0500

    Cleanup method expansion

--- frontend/src/views/pages/QuestApiExplorer.vue
+++ frontend/src/views/pages/QuestApiExplorer.vue
@@ -61,26 +61,49 @@
                     <!-- to be on the same line for proper pre formatting -->
                     <div
                       class="highlight html bg-dark hljs mb-4 code-display"
-                      style="padding-left: 20px !important; padding-top: 10px !important; padding-bottom: 10px !important"
+                      style="padding-left: 20px !important; padding-top: 10px !important; padding-bottom: 10px !important; white-space:nowrap"
                       v-if="apiMethods.length > 0">
-                      <div
-                        v-for="(method, index) in apiMethods"
-                        :key="index"><span v-if="method.methodPrefix" style="color:#9CDCFE">{{
-                          method.methodPrefix
-                        }}</span> <span v-if="!linkedExamples[languageSelection][method.method + '(']">{{
-                          method.method
-                        }}</span><a
-                        @click="loadExamples(method.method)" href="javascript:void(0);"
-                        v-if="linkedExamples[languageSelection][method.method + '(']">{{ method.method }}</a>({{
-                          method.params.join(", ")
-                        }}); <span
-                          v-if="method.comments" style="color: #57A64A">{{ method.comments }}</span> <span
-                          style="color: #57A64A"
-                          v-if="linkedExamples[languageSelection][method.method + '('] && linkedExamples[languageSelection][method.method + '('].length > 0">{{
-                            (languageSelection === "perl" ? "#" : "--")
-                          }} {{
-                            linkedExamples[languageSelection][method.method + "("].length
-                          }} Example(s)</span></div>
+
+                      <div v-for="(method, index) in apiMethods" :key="index">
+
+                        <!-- Method Prefix: eg $client quest:: eq. etc. -->
+                        <div v-if="method.methodPrefix" style="color:#9CDCFE;" class="d-inline-block">
+                          {{ method.methodPrefix }}
+                        </div>
+
+                        <!-- Method signature: Display if no linked example -->
+                        <div v-if="!linkedExamples[languageSelection][method.method + '(']" class="d-inline-block">
+                          {{ method.method }}
+                        </div>
+
+                        <!-- Method signature: With linked examples -->
+                        <div class="d-inline-block">
+                          <a
+                            @click="loadExamples(method.method)" href="javascript:void(0);"
+                            class="d-inline-block"
+                            v-if="linkedExamples[languageSelection][method.method + '(']">
+                            {{ method.method }}
+                          </a>
+
+                          <!-- Show method signature eg: (uint8 exp_percentage, uint8 max_level = 0, bool ignore_mods = false) -->
+                          <div class="d-inline-block">({{ method.params.join(", ") }});</div>
+
+                          <!-- Code Comments -->
+                          <div v-if="method.comments" style="color: #57A64A" class="d-inline-block ml-3">
+                            {{ method.comments }}
+                          </div>
+
+                          <!-- Display how many examples in comments -->
+                          <div
+                            style="color: #57A64A"
+                            class="d-inline-block ml-3"
+                            v-if="linkedExamples[languageSelection][method.method + '('] && linkedExamples[languageSelection][method.method + '('].length > 0">
+                            {{ (languageSelection === "perl" ? "#" : "--") }}
+                            {{ linkedExamples[languageSelection][method.method + "("].length }} Example(s)
+                          </div>
+
+                        </div>
+                      </div>
                     </div>
                   </div>
 
