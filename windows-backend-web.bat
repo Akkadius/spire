5ba2cde25a2f28735d8597c6caf7f7d236d27f88
Chris Miles <akkadius1@gmail.com>
2021-08-22 21:52:21 -0700

    Update windows-backend-web.bat

--- windows-backend-web.bat
+++ windows-backend-web.bat
@@ -2,4 +2,4 @@ TASKKILL /IM air.exe /F
 TASKKILL /IM spire.exe /F
 go install github.com/cosmtrek/air@v1.26.0
 
-"C:\Program Files\Git\git-bash.exe" -c 'air -c .air.windows.toml; sleep 10'
+"C:\Program Files\Git\git-bash.exe" -c 'air -c .air.windows.toml'
