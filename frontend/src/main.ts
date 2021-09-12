43c107d4e8485869c7cf2d41d0ab7fe2aa04622e
Chris Miles <akkadius1@gmail.com>
2021-09-11 20:38:44 -0700

    [Assets] Update FA

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -20,7 +20,7 @@ import './assets/css/global.css'
 import '@/components/eq-ui/styles/eq-ui.css'
 
 // font awesome
-import '@/assets/font-awesome-4.7.0/css/font-awesome.min.css'
+import '@/assets/font-awesome-4.7.0/css/font-awesome.css'
 
 // Lazyload
 import VueLazyload      from 'vue-lazyload'
