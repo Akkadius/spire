8c2fe4ee8bc61f9b5d002270e7a6ab34f8bae63d
Akkadius <akkadius1@gmail.com>
2021-09-19 03:04:09 -0500

    Refactor of spell logic, table view

---
+++ frontend/src/components/eq-ui/EQSpellDescription.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="spell['descnum'] > 0 && effectDescription !== ''">
+    {{ effectDescription }}
+  </div>
+</template>
+
+<script>
+import {Spells} from "@/app/spells/spells";
+import EqItemPreview from "@/components/eq-ui/EQItemPreview.vue";
+import EqSpellPreview from "@/components/eq-ui/EQSpellPreview.vue";
+import EqWindow from "@/components/eq-ui/EQWindow.vue";
+
+export default {
+  name: "EqSpellDescription",
+  components: {
+    "v-runtime-template": () => import("v-runtime-template"),
+    EqItemPreview,
+    EqSpellPreview,
+    EqWindow
+  },
+  created() {
+    Spells.getSpellDescription(this.spell).then((result) => {
+      if (result && result.trim() !== "") {
+        this.effectDescription = result;
+      }
+    })
+  },
+  data() {
+    return {
+      effectDescription: ""
+    }
+  },
+  props: {
+    spell: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
