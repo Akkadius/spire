5dde9b74c581e28cbf5ba402b3ecde132cbdc74c
Akkadius <akkadius1@gmail.com>
2021-11-29 03:00:22 -0600

    Update build-release.sh

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -5,13 +5,13 @@ cwd=$(pwd)
 # packr for packing web assets into binary
 go install github.com/gobuffalo/packr/packr
 
-#:: Build SPA (Frontend)
-cd "$cwd/frontend" && npm install && npm run build
-
 # Copy local asset images (This step is part of install now)
 curl --compressed -o /tmp/assets.zip -L https://github.com/Akkadius/eq-asset-preview/archive/refs/heads/master.zip
 unzip -o /tmp/assets.zip -d /tmp/assets
 cp -R /tmp/assets/eq-asset-preview-master/ ./frontend/public/
+
+#:: Build SPA (Frontend)
+cd "$cwd/frontend" && npm install && npm run build
 
 #:: Pack frontend assets into binary
 cd "$cwd" && packr clean
