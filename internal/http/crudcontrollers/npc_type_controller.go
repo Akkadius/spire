9a03fb1f2d6b655109dd72f83fb067187a65b77a
Akkadius <akkadius1@gmail.com>
2022-02-17 02:03:08 -0600

    Add support for nested key CRUD update calls

--- internal/http/crudcontrollers/npc_type_controller.go
+++ internal/http/crudcontrollers/npc_type_controller.go
@@ -1,10 +1,10 @@
 package crudcontrollers
 
 import (
+	"fmt"
 	"github.com/Akkadius/spire/internal/database"
 	"github.com/Akkadius/spire/internal/http/routes"
 	"github.com/Akkadius/spire/internal/models"
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 	"net/http"
@@ -12,7 +12,7 @@ import (
 )
 
 type NpcTypeController struct {
-	db	 *database.DatabaseResolver
+	db     *database.DatabaseResolver
 	logger *logrus.Logger
 }
 
@@ -21,7 +21,7 @@ func NewNpcTypeController(
 	logger *logrus.Logger,
 ) *NpcTypeController {
 	return &NpcTypeController{
-		db:	 db,
+		db:     db,
 		logger: logger,
 	}
 }
@@ -125,26 +125,44 @@ func (e *NpcTypeController) getNpcType(c echo.Context) error {
 // @Failure 500 {string} string "Error updating entity"
 // @Router /npc_type/{id} [patch]
 func (e *NpcTypeController) updateNpcType(c echo.Context) error {
-	npcType := new(models.NpcType)
-	if err := c.Bind(npcType); err != nil {
+	request := new(models.NpcType)
+	if err := c.Bind(request); err != nil {
 		return c.JSON(
 			http.StatusInternalServerError,
 			echo.Map{"error": fmt.Sprintf("Error binding to entity [%v]", err.Error())},
 		)
 	}
 
-	entity := models.NpcType{}
-	err := e.db.Get(models.NpcType{}, c).Model(&models.NpcType{}).First(&entity, npcType.ID).Error
-	if err != nil || npcType.ID == 0 {
-		return c.JSON(http.StatusNotFound, echo.Map{"error": "Cannot find entity"})
+	var params []interface{}
+	var keys []string
+
+	// primary key param
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Cannot find param [ID]"})
+	}
+	params = append(params, id)
+	keys = append(keys, "id = ?")
+
+	// query builder
+	var result models.NpcType
+	query := e.db.QueryContext(models.NpcType{}, c)
+	for i, _ := range keys {
+		query = query.Where(keys[i], params[i])
 	}
 
-	err = e.db.Get(models.NpcType{}, c).Model(&entity).Select("*").Updates(&npcType).Error
+	// grab first entry
+	err = query.First(&result).Error
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": fmt.Sprintf("Cannot find entity [%s]", err.Error())})
+	}
+
+	err = query.Select("*").Updates(&request).Error
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": fmt.Sprintf("Error updating entity [%v]", err.Error())})
 	}
 
-	return c.JSON(http.StatusOK, npcType)
+	return c.JSON(http.StatusOK, request)
 }
 
 // createNpcType godoc
