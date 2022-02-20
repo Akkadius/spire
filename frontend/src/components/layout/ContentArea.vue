a627c5544d17c3a61304e4dc167ca535eac8b883
Akkadius <akkadius1@gmail.com>
2022-02-19 21:06:07 -0600

    Add ContentArea component

---
+++ frontend/src/components/layout/ContentArea.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-fluid">
+    <div class="panel-body">
+      <div class="panel panel-default">
+        <div class="row">
+          <div class="col-12">
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentArea"
+}
+</script>
+
+<style scoped>
+
+</style>
