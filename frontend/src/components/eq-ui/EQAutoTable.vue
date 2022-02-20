3c8043b2928318bee54ab4d81605ed1536fcbbc1
Akkadius <akkadius1@gmail.com>
2022-02-19 21:06:16 -0600

    Format EQAutoTable

--- frontend/src/components/eq-ui/EQAutoTable.vue
+++ frontend/src/components/eq-ui/EQAutoTable.vue
@@ -1,12 +1,13 @@
 <template>
   <div class='eq-window-nested-blue' style="width: 100%; overflow-y: scroll">
-
     <div v-if="data && data.length === 0" class="p-3">
       There were no entries to be shown
     </div>
 
-    <table class="eq-table eq-highlight-rows"
-           style="display: table; font-size: 14px; overflow-x: scroll " v-if="data && data.length > 0">
+    <table
+      class="eq-table eq-highlight-rows"
+      style="display: table; font-size: 14px; overflow-x: scroll " v-if="data && data.length > 0"
+    >
       <thead>
       <tr>
         <th v-for="header in Object.keys(data[0])" v-if="typeof header !== 'object'">{{ header }}</th>
@@ -14,7 +15,9 @@
       </thead>
       <tbody>
       <tr v-for="(row, index) in data" :key="index">
-        <td style="width: 1%; white-space: nowrap;" v-for="key in Object.keys(row)" v-if="typeof row[key] !== 'object'">{{ row[key] }}</td>
+        <td style="width: 1%; white-space: nowrap;" v-for="key in Object.keys(row)" v-if="typeof row[key] !== 'object'">
+          {{ row[key] }}
+        </td>
       </tr>
       </tbody>
     </table>
