20d0cee2ead5a254483c4647c5923de093783edb
Akkadius <akkadius1@gmail.com>
2022-02-24 20:41:23 -0600

    Exclude objects from auto table

--- frontend/src/components/eq-ui/EQAutoTable.vue
+++ frontend/src/components/eq-ui/EQAutoTable.vue
@@ -6,16 +6,25 @@
 
     <table
       class="eq-table eq-highlight-rows"
-      style="display: table; font-size: 14px; overflow-x: scroll " v-if="data && data.length > 0"
+      style="display: table; font-size: 14px; overflow-x: scroll "
+      v-if="data && data.length > 0"
     >
       <thead>
       <tr>
-        <th v-for="header in Object.keys(data[0])" v-if="typeof header !== 'object'">{{ header }}</th>
+        <th
+          v-for="header in Object.keys(data[0])"
+          v-if="!doesColumnHaveObjects(header)"
+        >{{ header }}
+        </th>
       </tr>
       </thead>
       <tbody>
       <tr v-for="(row, index) in data" :key="index">
-        <td style="width: 1%; white-space: nowrap;" v-for="key in Object.keys(row)" v-if="typeof row[key] !== 'object'">
+        <td
+          style="width: 1%; white-space: nowrap;"
+          v-for="key in Object.keys(row)"
+          v-if="(typeof row[key] !== 'undefined') && !(typeof row[key] === 'object' && row[key] !== null && Object.keys(row[key]))"
+        >
           {{ row[key] }}
         </td>
       </tr>
@@ -30,6 +39,17 @@ export default {
   props: {
     data: {
       required: false
+    }
+  },
+  methods: {
+    doesColumnHaveObjects(column) {
+      if (typeof column === 'object') {
+        return true
+      }
+
+      return this.data.find((row) => {
+        return typeof row[column] === 'object' && row[column] !== null && Object.keys(row[column])
+      })
     }
   }
 }
