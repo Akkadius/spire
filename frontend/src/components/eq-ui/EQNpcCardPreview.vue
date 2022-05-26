8f4db5ed8ab27ff8a15015b0677c8a4b488cd3e3
Akkadius <akkadius1@gmail.com>
2022-05-25 22:03:13 -0500

    Safety accessors

--- frontend/src/components/eq-ui/EQNpcCardPreview.vue
+++ frontend/src/components/eq-ui/EQNpcCardPreview.vue
@@ -406,7 +406,7 @@ export default {
       }
 
       // loot
-      if (this.npc.loottable_id > 0 && this.npc.loottable.loottable_entries) {
+      if (this.npc.loottable_id > 0 && this.npc.loottable && this.npc.loottable.loottable_entries) {
         let lootItems = []
         for (let l of this.npc.loottable.loottable_entries) {
           // console.log(l)
