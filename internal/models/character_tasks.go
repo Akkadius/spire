6e56a3e6c657029a798a4d8e3266591dc21bef3d
Akkadius <akkadius1@gmail.com>
2022-07-23 15:58:07 -0500

    Update models

--- internal/models/character_tasks.go
+++ internal/models/character_tasks.go
@@ -10,6 +10,7 @@ type CharacterTask struct {
 	Slot         uint      `json:"slot" gorm:"Column:slot"`
 	Type         int8      `json:"type" gorm:"Column:type"`
 	Acceptedtime null.Uint `json:"acceptedtime" gorm:"Column:acceptedtime"`
+	WasRewarded  int8      `json:"was_rewarded" gorm:"Column:was_rewarded"`
 }
 
 func (CharacterTask) TableName() string {
