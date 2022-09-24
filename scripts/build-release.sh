c65a217355660be8d773cabcdff3008d76d1b921
Chris Miles <akkadius1@gmail.com>
2022-09-23 20:25:32 -0500

    [CI] Don't build changes when versions are same with release

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -2,8 +2,11 @@
 
 cwd=$(pwd)
 
+# shellcheck disable=SC2046
+if [ $(curl -s "https://api.github.com/repos/Akkadius/spire/tags" | jq -r '.[0].name' | sed 's/v//') = $(cat package.json | jq -r '.version') ]; then echo "Version tag is same as latest release exiting build"; exit; else echo "Local version different from remote, building..."; fi
+
 # packr for packing web assets into binary
-go install github.com/gobuffalo/packr/packr
+#go install github.com/gobuffalo/packr/packr
 
 # Copy local asset images (This step is part of install now)
 make build-assets
