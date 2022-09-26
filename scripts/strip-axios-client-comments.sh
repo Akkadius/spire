191648971e89789fa97f538f7362f049cd90437d
Akkadius <akkadius1@gmail.com>
2022-09-26 01:01:39 -0500

    [API Pipeline] Fix race condition in comment stripping script

--- scripts/strip-axios-client-comments.sh
+++ scripts/strip-axios-client-comments.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-find ./frontend/src/app/api/api/ -type f -name \*.ts | while read f; do cat $f | sed -n '/^\/\*.*\*\//!p' | sed -n '/ \/\/.*/!p' | sed 's|/\*|\n&|g;s|*/|&\n|g' | sed '/\/\*/,/*\//d' | sed '/^$/d' | sed '/^[[:space:]]*$/d' | tee $f; done
+find ./frontend/src/app/api/api/ -type f -name \*.ts | while read f; do cat $f | sed -n '/^\/\*.*\*\//!p' | sed -n '/ \/\/.*/!p' | sed 's|/\*|\n&|g;s|*/|&\n|g' | sed '/\/\*/,/*\//d' | sed '/^$/d' | sed '/^[[:space:]]*$/d' | tee $f.bak; done
+
+find ./frontend/src/app/api/api/ -name "*.bak" -exec sh -c 'mv -f $0 ${0%.bak}' {} \;
