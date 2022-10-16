51dfc960d1086d99a9598937d9df32df7d5147fd
Akkadius <akkadius1@gmail.com>
2022-10-15 21:31:40 -0500

    Add banner

---
+++ internal/banner/banner.go
@@ -0,0 +1,34 @@
+package banner
+
+import "fmt"
+
+func Print() {
+	fmt.Println(`                                                                                                    
+                                                                                            
+                                              ^!!:                                          
+       .^!????7~^.                           !BBBB^                                         
+     :JPP555PBBBBY.                          .7YJ7.                                         
+    !GB7     ^JGY:                                                                          
+   .GGG:       :          .    :!~:            .^        .^    .^!!^      .:^^^:            
+   :GGGP~              .~5G^  ^PBGG57.       .!5B?    .^JPB~  7GBBB!    ~JYJ5GGGP:          
+    ?BGGGY~.         ^J5BBB7.?GGGBBBBP!    :?5BGBY   ^JGBGB7.YPYJY7   :5P~   JBB5.          
+     !PBBBBP?:       .:^GGGPJ!^:^!YGGGG!     :PGBY     ~GGG55~       :PG^   :5P7            
+      .~YGBBBGY~.      .PGGP.      ~GGGG^    .PGBY     :GGGG^        5B?  :JP?.             
+         :?PBBBBP!     .PGBY        !GGBJ    .GGBY     :GGBY        ~BG^.?P?.               
+           .~YGBGB?    .PGBY        .PGG5    :GGBY     :GGB?        ?BG?5J:                 
+     .        ^YGGG^    PGBY         5GBY    :GGBY     :GGB7        ?BG5.                   
+    ?Y          YBG^    5GBY        :GGB7    ^GGBY     :GGBJ        !BGG~                   
+   ?BG!         ?BY     5GB5        JBB5.    ~BGGP^    ^GGGG!.      .5BGGJ~:...:7^          
+  ?BGGG?^.   .^JGJ.     5GGG!     .?BB5.     7BGGBG^   ^BGGGB!       :PBGBBGPPPGY.          
+  YGBBBBBGPPPGGY~       5BGGP5?!7JPBP7       !BBBG!    .5BBG!         :YGBBBBBG7            
+   ^!?JJJJJ?!^.         5BGP:~?JJ?!^          !Y?:      .7?^            :7JJJ7:             
+                        YBGG:                                                               
+                        YBGB~                                                               
+                        YBGB7                                                               
+                        YB5~                                                                
+                        :^                                                                  
+                                                                                             `)
+	fmt.Printf("\n\n")
+	fmt.Printf(" A rich, portable server editing and development toolkit for EverQuest Emulator servers\n")
+	fmt.Printf("\n")
+}
