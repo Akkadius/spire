2c67ee11dfe37c10a8706e5f781e1a35ef610934
Aeadoin <109764533+Aeadoin@users.noreply.github.com>
2022-11-09 16:58:18 -0500

    File additions for Bots.

---
+++ frontend/src/components/preview/BotSpellPreview.vue
@@ -0,0 +1,274 @@
+<template>
+  <div v-if="spells">
+    <div
+      class="mt-3 text-center font-weight-bold p-3"
+      v-if="spells && !spells.bot_spells_entries"
+    >
+      No spells found in table
+    </div>
+
+    <table
+      class="eq-table bordered eq-highlight-rows"
+      style="font-size: 14px; "
+      v-if="spells && spells.bot_spells_entries"
+      id="bot-spell-preview-list-table"
+    >
+      <thead class="eq-table-floating-header">
+      <tr>
+        <th v-if="editButtons"></th>
+        <th>Spell</th>
+        <th>Type</th>
+        <th>Mana</th>
+        <th>Priority</th>
+        <th>Recast</th>
+        <th>Min Lvl</th>
+        <th>Max Lvl</th>
+        <th>Bucket Name</th>
+        <th>Bucket Value</th>
+        <th>Bucket Comparison</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+        v-for="e in spellsList"
+        :id="'spell-list-entry-' + e.id"
+        :key="'spell-' + e.id"
+        :class="(highlightedSpell && e.id === highlightedSpell ? 'pulsate-highlight-white' : '')"
+        :style="(e.is_parented ? 'background-color: rgb(0 255 255 / 20%);' : '')"
+      >
+        <td
+          class="text-center pl-0 pr-0"
+          style="min-width: 80px;"
+          v-if="editButtons"
+        >
+          {{(e.is_parented ? `Parent List (${e.bot_spells_id})` : '')}}
+
+          <b-button
+            variant="primary"
+            class="btn-dark btn-sm btn-outline-success"
+            style="padding: 0px 6px;"
+            title="Edit spell entry"
+            @click="editSpellListEntry(e)"
+            v-if="!e.is_parented"
+          >
+            <i class="fa fa-pencil-square"></i>
+          </b-button>
+
+          <b-button
+            variant="primary"
+            class="btn btn-dark btn-sm btn-outline-danger ml-1 btn-primary"
+            style="padding: 0px 6px;"
+            title="Delete spell entry"
+            @click="deleteSpellListEntry(e)"
+            v-if="!e.is_parented"
+          >
+            <i class="fa fa-trash"></i>
+          </b-button>
+        </td>
+
+        <td style="min-width: 220px">
+          <spell-popover
+            :spell="e.spells_new"
+            :size="20"
+            v-if="Object.keys(e.spells_new).length > 0 && e.spells_new"
+            class="mt-2"
+          />
+        </td>
+        <td>{{ (BOT_SPELL_TYPES[e.type] ? BOT_SPELL_TYPES[e.type] : "") }}</td>
+        <td>{{ getManacost(e) }}</td>
+        <td>{{ e.priority }}</td>
+        <td>{{ getRecastDelay(e) }}s</td>
+        <td>{{ e.minlevel }}</td>
+        <td>{{ e.maxlevel }}</td>
+        <td>{{ e.bucket_name }}</td>
+        <td>{{ e.bucket_value }}</td>
+        <td>{{ e.bucket_comparison }}</td>
+      </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+import SpellPopover        from "../SpellPopover";
+import EqDebug             from "../eq-ui/EQDebug";
+import {BOT_SPELL_TYPES}   from "../../app/constants/eq-bot-spells";
+import {SpireQueryBuilder} from "../../app/api/spire-query-builder";
+import {NpcSpellApi} from "../../app/api";
+import {SpireApi}    from "../../app/api/spire-api";
+import Tablesort     from "../../app/utility/tablesort";
+import {BotSpellsEntryApi} from "../../app/api/api/bot-spells-entry-api";
+
+export default {
+  name: "BotSpellPreview",
+  components: { EqDebug, SpellPopover },
+  data() {
+    return {
+      BOT_SPELL_TYPES: BOT_SPELL_TYPES,
+
+      spellsList: [],
+
+      fields: {
+        "type": 0,
+        "minlevel": 1,
+        "maxlevel": 255,
+        "manacost": -1,
+        "recast_delay": -1,
+        "priority": 0,
+        "resist_adjust": null,
+        "bucket_name": "",
+        "bucket_value": "",
+        "bucket_comparison": 0,
+      }
+    }
+  },
+  watch: {
+    spells: {
+      deep: true,
+      async handler() {
+        this.init()
+      }
+    },
+    highlightedSpell: {
+      handler() {
+
+      }
+    }
+  },
+  props: {
+    spells: {
+      type: Object,
+      required: false
+    },
+    editButtons: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    highlightedSpell: {
+      type: [Number, String],
+      required: false,
+      default: 0
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+
+    editSpellListEntry(e) {
+      console.log("edit spell", e)
+      this.$emit("edit-spell", e);
+    },
+
+    async deleteSpellListEntry(e) {
+      if (confirm(`Are you sure you want to delete this spell? \n\n(${e.spells_new.name}) (${e.spells_new.id})?`)) {
+
+        try {
+          await (new BotSpellsEntryApi(...SpireApi.cfg()))
+            .deleteBotSpellsEntry({ id: e.id })
+            .then(() => {
+              this.$emit("reload-parent", true);
+              this.$emit("notification", "Spell deleted successfully!");
+            })
+        } catch (err) {
+          if (err.response.data.error) {
+            if (err.response && err.response.data && err.response.data.error) {
+              this.$emit("error", err.response.data.error);
+            }
+          }
+        }
+      }
+    },
+
+    async init() {
+      console.log("spell preview watcher")
+
+      let spellsList = this.spells && this.spells.bot_spells_entries && this.spells.bot_spells_entries.length > 0
+        ? JSON.parse(JSON.stringify(this.spells.bot_spells_entries))
+        : []
+
+      if (this.spells.parent_list > 0) {
+        try {
+          const BotSpellsClient = (new NpcSpellApi(...SpireApi.cfg()))
+          const r               = await BotSpellsClient.getNpcSpell({ id: this.spells.parent_list },
+            {
+              query:
+                (new SpireQueryBuilder())
+                  .includes([
+                    "BotSpellsEntries.SpellsNew",
+                  ])
+                  .limit(100000)
+                  .get()
+            }
+          )
+
+          for (const [i, e] of r.data.bot_spells_entries.entries()) {
+            r.data.bot_spells_entries[i].is_parented = true
+          }
+
+          if (r.status === 200 && r.data && r.data.bot_spells_entries) {
+            spellsList = spellsList.concat(r.data.bot_spells_entries)
+          }
+        } catch (err) {
+          if (err.response.data.error) {
+            console.log("Error fetching parent list", err)
+          }
+        }
+      }
+
+      this.spellsList = spellsList
+        .sort((a, b) => {
+          if (a.minlevel === b.minlevel) {
+            return a.spells_new &&
+              b.spells_new &&
+              b.spells_new.name &&
+              a.spells_new.name &&
+              a.spells_new.name.localeCompare(b.spells_new.name)
+          }
+
+          return (b.minlevel < a.minlevel) ? 1 : -1
+        })
+
+      // table sort
+      const target = document.getElementById('bot-spell-preview-list-table')
+      if (target) {
+        setTimeout(() => {
+          new Tablesort(target);
+        }, 100)
+      }
+
+    },
+    getRecastDelay(e) {
+      if (e.recast_delay === -2) {
+        return 0
+      }
+      if (e.recast_delay === -1) {
+        return e.spells_new.recast_time / 1000
+      }
+
+      return e.recast_delay / 1000
+    },
+    getManacost(e) {
+      if (e.manacost === -1) {
+        return e.spells_new.mana
+      }
+
+      return e.manacost
+    },
+    toTitleCase(str) {
+      return str.replace(
+        /\w\S*/g,
+        function (txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+      );
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/bots/BotSpellsEditor.vue
@@ -0,0 +1,457 @@
+<template>
+  <content-area style="padding: 0px !important">
+    <div class="row">
+      <div :class="(isSubEditActive() ? 'col-4' : 'col-12')">
+        <eq-window
+          title="Bot Spells"
+          class="p-0"
+        >
+          <div class="row minified-inputs mt-4 text-center">
+            <div class="col-3 p-0 text-right">
+              <div class="d-inline-block btn-group ml-4 text-right" role="group" style="margin-top: 26px">
+                <b-button
+                  size="sm"
+                  variant="outline-warning"
+                  @click="zeroState();"
+                >
+                  <i class="fa fa-refresh mr-1"></i>
+                  Reset
+                </b-button>
+              </div>
+            </div>
+
+            <div class="col-4">
+              Search
+              <b-form-input
+                v-model="search"
+                v-on:keyup="doSearch()"
+                placeholder="Search by text or id"
+              />
+            </div>
+
+            <div class="col-2" v-if="loading">
+              <div class="text-center" style="margin-top: 17px">
+                Loading...
+                <loader-fake-progress/>
+              </div>
+            </div>
+          </div>
+
+          <div
+            id="bot-spell-viewport"
+            style="max-height: 80vh; overflow-y: scroll"
+          >
+            <table
+              class="eq-table bordered eq-highlight-rows row-table bot-spell-sets-table"
+              style="display: table; font-size: 14px; overflow-x: scroll"
+              v-if="rows && rows.length > 0"
+              id="bot-spell-sets-table"
+            >
+              <thead
+                class="eq-table-floating-header"
+              >
+              <tr>
+                <th style="width: 100px"></th>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Spell Count</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr
+                :id="'row-' + e.id"
+                v-for="(e, index) in rows"
+                :key="e.id"
+                :class="isRowSelected(e) ? 'pulsate-highlight-white' : ''"
+              >
+                <td class="text-center pl-0 pr-0">
+                  <b-button
+                    variant="primary"
+                    class="btn-dark btn-sm btn-outline-success"
+                    style="padding: 0px 6px;"
+                    title="Edit Spell Set"
+                    @click="editBotSpellSet(e.id)"
+                  >
+                    <i class="fa fa-pencil-square"></i>
+                  </b-button>
+
+                  <b-button
+                    variant="primary"
+                    class="btn-dark btn-sm btn-outline-white ml-1"
+                    style="padding: 0px 6px;"
+                    title="View Spell Set"
+                    @click="selectSpellSet(e)"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </b-button>
+                </td>
+                <td class="text-center">{{ e.id }}</td>
+                <td>{{ e.name }}</td>
+              
+                <td>{{ getSpellCount(e) }}</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <!-- Pagination -->
+          <div class="row text-center justify-content-center">
+            <div class="col-12 text-center mt-3">
+              <b-pagination
+                :key="currentPage"
+                class="mb-3"
+                v-model="currentPage"
+                :total-rows="totalRows"
+                :hide-ellipsis="true"
+                :per-page="pageSize"
+                @change="paginate"
+              />
+            </div>
+          </div>
+
+        </eq-window>
+      </div>
+
+      <!-- Preview Pane -->
+      <div class="col-8 fade-in" v-if="isSubEditActive()">
+        <eq-window
+          v-if="selectedSpellSet && selectedSpellSet.bot_spells_entries && selectedSpellSet.bot_spells_entries.length"
+          :title="`Bot Spells ID (${selectedSpellSet.id}) [${selectedSpellSet.name}] Count (${selectedSpellSet.bot_spells_entries.length})`"
+          class="p-2"
+        >
+          <div style="max-height: 88vh; overflow-y: scroll; overflow-x: hidden">
+            <bot-spell-preview
+              :spells="selectedSpellSet"
+            />
+          </div>
+        </eq-window>
+      </div>
+    </div>
+
+  </content-area>
+</template>
+
+<script>
+import EqWindowSimple      from "../../components/eq-ui/EQWindowSimple";
+import EqAutoTable         from "../../components/eq-ui/EQAutoTable";
+import ContentArea         from "../../components/layout/ContentArea";
+import {NpcSpellApi}      from "../../app/api";
+import {SpireApi}         from "../../app/api/spire-api";
+import LoaderFakeProgress from "../../components/LoaderFakeProgress";
+import {ROUTE}             from "../../routes";
+import {SpireQueryBuilder} from "../../app/api/spire-query-builder";
+import InfoErrorBanner     from "../../components/InfoErrorBanner";
+import EqWindow            from "../../components/eq-ui/EQWindow";
+import Tablesort           from "../../app/utility/tablesort";
+import {scrollToTarget}    from "../../app/utility/scrollToTarget";
+import {debounce}          from "../../app/utility/debounce";
+import BotSpellPreview     from "../../components/preview/BotSpellPreview";
+import EqDebug             from "../../components/eq-ui/EQDebug";
+import util                from "util";
+
+const BotSpellsClient = (new NpcSpellApi(...SpireApi.cfg()))
+
+export default {
+  name: "BotSpellsEditor",
+  components: {
+    EqDebug,
+    BotSpellPreview,
+    EqWindow,
+    InfoErrorBanner,
+    LoaderFakeProgress: LoaderFakeProgress,
+    ContentArea,
+    EqAutoTable,
+    EqWindowSimple,
+    "v-runtime-template": () => import("v-runtime-template")
+  },
+  data() {
+    return {
+
+      // pagination (all)
+      lastPage: 0, // this keeps track of last page in state
+      currentPage: 1,
+      totalRows: 0,
+      pageSize: 25,
+
+      // table
+      rows: [],
+      filterColumns: [],
+
+      // for the sub selector pane on the right
+      subSelectedId: -1,
+      selectedSpellSet: {},
+
+      // api responses
+      error: "",
+      notification: "",
+
+      // selection
+      search: "",
+
+      lastSelectedTime: Date.now(),
+
+      loading: false, // are we loading or not
+    }
+  },
+
+  watch: {
+    '$route'() {
+      console.log("route trigger")
+      this.reset()
+      this.init()
+    },
+  },
+
+  methods: {
+
+    zeroState() {
+      this.reset()
+      // lastPage: 0, // this keeps track of last page in state
+      //   currentPage: 1,
+      this.lastPage = 0;
+
+      this.loading = true;
+      this.updateQueryState();
+      this.init()
+    },
+
+    editBotSpellSet(id) {
+      this.$router.push(
+        {
+          path: util.format(ROUTE.BOT_SPELL_EDIT, id)
+        }
+      ).catch(() => {
+      })
+    },
+
+    getSpellCount(e) {
+      return e.bot_spells_entries ? e.bot_spells_entries.length : 0
+    },
+
+    paginate() {
+      // models aren't quite updated when we trigger this so queue the pagination
+      setTimeout(() => {
+        console.log("We're paginating")
+        console.log(this.currentPage)
+        console.log(this.totalRows)
+        this.updateQueryState()
+      }, 100)
+    },
+
+    doSearch: debounce(function () {
+      this.subSelectedId = -1;
+      this.rows          = [];
+      this.loading       = true;
+      this.lastPage      = 0;
+      this.updateQueryState()
+    }, 600),
+
+    getColumnWidth(field) {
+      if (field === 'event_') {
+        return 'width: 130px;'
+      }
+      if (field === 'type') {
+        return 'width: 140px;'
+      }
+
+      return ''
+    },
+
+    /**
+     * Resets
+     */
+    reset() {
+      this.currentPage   = 1
+      this.search        = ""
+      this.error         = ""
+      this.notification  = ""
+      this.subSelectedId = -1
+    },
+    resetSelections() {
+      this.subSelectedId = -1
+    },
+
+    /**
+     * State
+     */
+    updateQueryState: function () {
+      let queryState = {};
+
+      if (this.subSelectedId !== -1) {
+        queryState.selectedId = this.subSelectedId
+      }
+      if (this.currentPage > 0) {
+        queryState.page = this.currentPage
+      }
+      if (this.search !== "") {
+        queryState.search = this.search
+      }
+
+      console.log("[bot-spells-editor] Updating query state", queryState)
+
+      this.$router.push(
+        {
+          path: ROUTE.BOT_SPELLS_EDIT,
+          query: queryState
+        }
+      ).catch(() => {
+      })
+    },
+
+    loadQueryState() {
+      console.log("loading query state")
+      if (this.$route.query.selectedId >= 0) {
+        this.subSelectedId = parseInt(this.$route.query.selectedId);
+      }
+
+      if (this.$route.query.search !== "") {
+        this.search = this.$route.query.search;
+      }
+      if (typeof this.$route.query.page !== 'undefined' && parseInt(this.$route.query.page) !== 0) {
+        this.currentPage = parseInt(this.$route.query.page);
+        console.log("current page", this.currentPage)
+      }
+
+      console.log("query params", this.$route.query)
+    },
+
+    /**
+     * Sub editor selection
+     */
+    selectSpellSet(e) {
+      this.subSelectedId = e.id
+      this.updateQueryState()
+    },
+
+    isSubEditActive() {
+      return this.subSelectedId >= 0
+    },
+
+    isRowSelected(e) {
+      return e.id === this.subSelectedId
+    },
+
+    /**
+     * Initialize
+     */
+    async init(reset = false) {
+      this.loadQueryState()
+
+      if (this.totalRows === 0) {
+        this.totalRows = await this.getBotSpellCount()
+      }
+
+      // console.log("last page", this.lastPage)
+      // console.log("current page", this.currentPage)
+
+      if (this.lastPage !== this.currentPage || reset) {
+        console.log("reloading content")
+        this.loading      = true
+        this.lastPage     = parseInt(this.currentPage)
+        this.rows         = await this.getBotSpells()
+        this.originalRows = JSON.parse(JSON.stringify(this.rows))
+      }
+
+      setTimeout(() => {
+        if (document.getElementById('bot-spell-sets-table')) {
+          new Tablesort(document.getElementById('bot-spell-sets-table'));
+        }
+      }, 100)
+
+      if (this.subSelectedId > 0) {
+        for (const e of this.rows) {
+          if (e.id === this.subSelectedId) {
+            this.selectedSpellSet = JSON.parse(JSON.stringify(e))
+          }
+        }
+      }
+
+      this.loading = false
+    },
+
+    isNumeric(value) {
+      return /^-?\d+$/.test(value);
+    },
+
+    async getBotSpellCount() {
+      let builder = (new SpireQueryBuilder())
+        .select(["id"])
+        .limit(100000)
+
+      if (this.search && this.search.length > 0) {
+        if (this.isNumeric(this.search)) {
+          builder.where("id", "=", this.search)
+        } else {
+          builder.where("name", "like", this.search)
+        }
+      }
+
+      // console.log("SEARCH IS (count) ", this.search)
+
+      const response = await BotSpellsClient.listNpcSpells(
+        builder.get()
+      )
+      if (response.status === 200 && response.data) {
+        return response.data.length
+      }
+
+      return 0
+    },
+
+    async getBotSpells() {
+      console.log("getbotspells", this.currentPage)
+
+      if (typeof this.$route.query.page !== 'undefined' && parseInt(this.$route.query.page) !== 0) {
+        this.currentPage = parseInt(this.$route.query.page);
+        console.log("current page", this.currentPage)
+      }
+
+      let builder = (new SpireQueryBuilder())
+        .page(this.currentPage)
+        .includes([
+          "BotSpellsEntries.SpellsNew",
+        ])
+        .where("id", ">=", 3001)
+        .where("id", "<=", 3016)
+        .limit(this.pageSize)
+
+      if (this.search && this.search.length > 0) {
+        if (this.isNumeric(this.search)) {
+          builder.where("id", "=", this.search)
+        } else {
+          builder.where("name", "like", this.search)
+        }
+      }
+
+      // console.log("SEARCH IS (data) ", this.search)
+
+      const response = await BotSpellsClient.listNpcSpells(
+        builder.get()
+      )
+      if (response.status === 200 && response.data) {
+        return response.data
+      }
+    },
+
+  },
+  created() {
+    this.originalRows = []
+  },
+  async mounted() {
+    await this.init()
+
+    if (this.subSelectedId > 0) {
+      scrollToTarget(
+        "bot-spell-viewport",
+        'row-' + this.subSelectedId
+      )
+    }
+  },
+}
+</script>
+
+<style>
+.bot-spell-sets-table td {
+  vertical-align: middle !important;
+}
+</style>
