464a1ea3d26ca57490a6ddb080e4104d3ccf7ae1
Aeadoin <109764533+Aeadoin@users.noreply.github.com>
2022-11-09 17:26:08 -0500

    Add Constants

---
+++ frontend/src/app/constants/eq-data-bucket-comparisons.ts
@@ -0,0 +1,12 @@
+export const DATA_BUCKET_COMPARISONS = {
+  0: "Equality",
+  1: "Inequality",
+  2: "Greater Than or Equal To",
+  3: "Lesser Than or Equal To",
+  4: "Greater Than",
+  5: "Lesser Than",
+  6: "Any (Pipe Seperated Values)",
+  7: "Not Any (Pipe Seperated Values)",
+  8: "Between (Pipe Seperated Values)",
+  9: "Not Between (Pipe Seperated Values)",
+}
