1fbfefd1572f59b6fa284d195a1fdea0e4616c94
Aeadoin <109764533+Aeadoin@users.noreply.github.com>
2022-11-09 17:39:39 -0500

    Update constants to match others

--- frontend/src/app/constants/eq-data-bucket-comparisons.ts
+++ frontend/src/app/constants/eq-data-bucket-comparisons.ts
@@ -1,6 +1,6 @@
 export const DATA_BUCKET_COMPARISONS = {
-  0: "Equality",
-  1: "Inequality",
+  0: "Equal To",
+  1: "Not Equal To",
   2: "Greater Than or Equal To",
   3: "Lesser Than or Equal To",
   4: "Greater Than",
