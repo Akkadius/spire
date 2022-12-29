1c5f97c816c07e914d0cb84688d31b7d3621c16d
Akkadius <akkadius1@gmail.com>
2022-12-29 06:32:11 -0600

    Update Navbar.vue

--- frontend/src/components/layout/Navbar.vue
+++ frontend/src/components/layout/Navbar.vue
@@ -41,7 +41,11 @@
       <router-link class="ml-3 mt-3" to="/">
         <h1 class="text-center eq-header small-mobile">
           Spire
+<!--          <h3 class="text-center eq-header small-mobile d-inline" style="font-size: 40px">-->
+<!--            [Admin]-->
+<!--          </h3>-->
         </h1>
+
 
       </router-link>
 
