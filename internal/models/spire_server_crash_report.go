4e52dab48797385d4d3668b508916e72b37b892e
Akkadius <akkadius1@gmail.com>
2023-01-23 02:06:05 -0600

    Schema tweak

--- internal/models/spire_server_crash_report.go
+++ internal/models/spire_server_crash_report.go
@@ -2,20 +2,20 @@ package models
 
 type CrashReport struct {
 	ID              uint    `json:"id" gorm:"primary_key,AUTO_INCREMENT"`
-	PlatformName    string  `json:"platform_name" gorm:"platform_name"`
-	CompileDate     string  `json:"compile_date" gorm:"compile_date"`
-	CompileTime     string  `json:"compile_time" gorm:"compile_time"`
+	PlatformName    string  `json:"platform_name" gorm:"platform_name;type:varchar(20)"`
+	CompileDate     string  `json:"compile_date" gorm:"compile_date;type:varchar(20)"`
+	CompileTime     string  `json:"compile_time" gorm:"compile_time;type:varchar(20)"`
 	Cpus            int     `json:"cpus" gorm:"cpus"`
 	CrashReport     string  `json:"crash_report" gorm:"crash_report"`
-	OsMachine       string  `json:"os_machine" gorm:"os_machine"`
-	OsRelease       string  `json:"os_release" gorm:"os_release"`
-	OsSysname       string  `json:"os_sysname" gorm:"os_sysname"`
-	OsVersion       string  `json:"os_version" gorm:"os_version"`
+	OsMachine       string  `json:"os_machine" gorm:"os_machine;type:varchar(200)"`
+	OsRelease       string  `json:"os_release" gorm:"os_release;type:varchar(200)"`
+	OsSysname       string  `json:"os_sysname" gorm:"os_sysname;type:varchar(200)"`
+	OsVersion       string  `json:"os_version" gorm:"os_version;type:varchar(200)"`
 	ProcessID       int     `json:"process_id" gorm:"process_id"`
 	RssMemory       float64 `json:"rss_memory" gorm:"rss_memory"`
-	ServerName      string  `json:"server_name" gorm:"server_name"`
-	ServerShortName string  `json:"server_short_name" gorm:"server_short_name"`
-	ServerVersion   string  `json:"server_version" gorm:"server_version"`
+	ServerName      string  `json:"server_name" gorm:"server_name;type:varchar(200)"`
+	ServerShortName string  `json:"server_short_name" gorm:"server_short_name;type:varchar(200)"`
+	ServerVersion   string  `json:"server_version" gorm:"server_version;type:varchar(50);index:version"`
 	Uptime          int     `json:"uptime" gorm:"uptime"`
 }
 
