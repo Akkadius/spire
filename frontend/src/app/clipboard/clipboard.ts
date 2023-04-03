53b6dfcceaa2d666baaff3ab330267dce1548b15
Akkadius <akkadius1@gmail.com>
2023-04-03 15:09:23 -0500

    Update clipboard.ts

--- frontend/src/app/clipboard/clipboard.ts
+++ frontend/src/app/clipboard/clipboard.ts
@@ -13,15 +13,19 @@ export default class Clipboard {
 
   static copyFromElement(element) {
     // @ts-ignore
-    const str = document.getElementById(element).innerText
-    const el  = document.createElement('textarea')
-    el.value  = str
-    el.setAttribute('readonly', '')
-    el.style.position = 'absolute'
-    el.style.left     = '-9999px'
-    document.body.appendChild(el)
-    el.select()
-    document.execCommand('copy')
-    document.body.removeChild(el)
+    const e = document.getElementById(element)
+    if (e) {
+      const str = e.innerText
+      const el  = document.createElement('textarea')
+      el.value  = str
+      el.setAttribute('readonly', '')
+      el.style.position = 'absolute'
+      el.style.left     = '-9999px'
+      document.body.appendChild(el)
+      el.select()
+      document.execCommand('copy')
+      document.body.removeChild(el)
+    }
+
   }
 }
