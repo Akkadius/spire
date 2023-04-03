f05bc24c7da5fabf2a69f22c99d36c9dbec20192
Akkadius <akkadius1@gmail.com>
2023-04-03 05:00:50 -0500

    Update NpcSpellListEditor.vue

--- frontend/src/views/npcs/NpcSpellListEditor.vue
+++ frontend/src/views/npcs/NpcSpellListEditor.vue
@@ -174,7 +174,7 @@
                 <b-form-input
                   v-if="field.fType === 'text'"
                   :id="field.field"
-                  v-model.number="editingSpellEntry[field.field]"
+                  v-model="editingSpellEntry[field.field]"
                   class="m-0 mt-1"
                   v-on="field.e ? getEventHandlers(field.e, field.field) : {}"
                   v-b-tooltip.hover.v-dark.right :title="getFieldDescription(field.field)"
