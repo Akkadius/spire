c61a1f04280cf06c0d3022404aff710512f4e327
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2023-05-29 15:18:44 -0400

    Update NpcSpecialAbilities.vue

--- frontend/src/components/tools/NpcSpecialAbilities.vue
+++ frontend/src/components/tools/NpcSpecialAbilities.vue
@@ -468,7 +468,7 @@
           <input
             type="text" @change="calculateSpecialAbilities" class="ability_check_sub form-control" style=""
             v-model="abilityParams[44][0]" value="" placeholder="0"
-            v-b-tooltip.hover title="% Chance to avoid melee via Riposte, Parry, Block, or Dodge"
+            v-b-tooltip.hover title="% Chance to avoid melee via Riposte, Parry, Block, and Dodge"
           >
         </td>
         <td>
