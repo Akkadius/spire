b5ac9e42cd3ae864c1e280b66be9544b0088141a
Akkadius <akkadius1@gmail.com>
2023-12-12 14:04:31 -0600

    Fix starting items - items query issue

--- internal/http/crudcontrollers/item_controller.go
+++ internal/http/crudcontrollers/item_controller.go
@@ -67,7 +67,7 @@ func (e *ItemController) listItems(c echo.Context) error {
 	var results []models.Item
 	err := e.db.QueryContext(models.Item{}, c).Find(&results).Error
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err})
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
 	}
 
 	return c.JSON(http.StatusOK, results)
--- internal/models/blocked_spells.go
+++ internal/models/blocked_spells.go
@@ -1,18 +1,26 @@
 package models
 
+import (
+	"github.com/volatiletech/null/v8"
+)
+
 type BlockedSpell struct {
-	ID          int     `json:"id" gorm:"Column:id"`
-	Spellid     uint32  `json:"spellid" gorm:"Column:spellid"`
-	Type        int8    `json:"type" gorm:"Column:type"`
-	Zoneid      int     `json:"zoneid" gorm:"Column:zoneid"`
-	X           float32 `json:"x" gorm:"Column:x"`
-	Y           float32 `json:"y" gorm:"Column:y"`
-	Z           float32 `json:"z" gorm:"Column:z"`
-	XDiff       float32 `json:"x_diff" gorm:"Column:x_diff"`
-	YDiff       float32 `json:"y_diff" gorm:"Column:y_diff"`
-	ZDiff       float32 `json:"z_diff" gorm:"Column:z_diff"`
-	Message     string  `json:"message" gorm:"Column:message"`
-	Description string  `json:"description" gorm:"Column:description"`
+	ID                     int         `json:"id" gorm:"Column:id"`
+	Spellid                uint32      `json:"spellid" gorm:"Column:spellid"`
+	Type                   int8        `json:"type" gorm:"Column:type"`
+	Zoneid                 int         `json:"zoneid" gorm:"Column:zoneid"`
+	X                      float32     `json:"x" gorm:"Column:x"`
+	Y                      float32     `json:"y" gorm:"Column:y"`
+	Z                      float32     `json:"z" gorm:"Column:z"`
+	XDiff                  float32     `json:"x_diff" gorm:"Column:x_diff"`
+	YDiff                  float32     `json:"y_diff" gorm:"Column:y_diff"`
+	ZDiff                  float32     `json:"z_diff" gorm:"Column:z_diff"`
+	Message                string      `json:"message" gorm:"Column:message"`
+	Description            string      `json:"description" gorm:"Column:description"`
+	MinExpansion           int8        `json:"min_expansion" gorm:"Column:min_expansion"`
+	MaxExpansion           int8        `json:"max_expansion" gorm:"Column:max_expansion"`
+	ContentFlags           null.String `json:"content_flags" gorm:"Column:content_flags"`
+	ContentFlagsDisabled   null.String `json:"content_flags_disabled" gorm:"Column:content_flags_disabled"`
 }
 
 func (BlockedSpell) TableName() string {
