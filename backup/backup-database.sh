caa32adbed1e90547474d6b5d232d52553be2284
Chris Miles <akkadius1@gmail.com>
2024-02-12 03:33:10 -0600

    Prod Backups (#172)
    * Backups
    
    * Update backup-database.sh
    
    * Update backup-database.sh
    
    * Update backup-database.sh

---
+++ backup/backup-database.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Fetch hostname deployment directory and format into backup prepend
+# DEPLOYMENT_NAME=$HOST_NAME/$(basename $HOST_DIR)
+
+CWD=$(pwd)
+source $CWD/.env
+
+cd /tmp/
+
+# validate
+set -e
+"$CWD/backup/validate-dropbox.sh"
+set +e
+
+#############################################
+# mysqldump
+#############################################
+echo "# Dumping database and compressing"
+MYSQL_BACKUP_NAME=${MYSQL_SPIRE_DATABASE}-$(date +"%m-%d-%Y")
+mysqldump --lock-tables=false -u${MYSQL_SPIRE_USERNAME} -p${MYSQL_SPIRE_PASSWORD} -h ${MYSQL_SPIRE_HOST} ${MYSQL_SPIRE_DATABASE} >/tmp/${MYSQL_BACKUP_NAME}.sql
+tar -zcvf ${MYSQL_BACKUP_NAME}.tar.gz ${MYSQL_BACKUP_NAME}.sql
+
+#############################################
+# upload
+#############################################
+echo "# Uploading database snapshot"
+dropbox_uploader.sh upload ${MYSQL_BACKUP_NAME}.tar.gz ${DEPLOYMENT_NAME:-backups}/database-snapshots/${MYSQL_BACKUP_NAME}.tar.gz
+
+#############################################
+# prune snapshots
+#############################################
+BACKUP_RETENTION=${BACKUP_RETENTION_DAYS_DB_SNAPSHOTS:-7}
+BACKUP_PATH=${DEPLOYMENT_NAME:-backups}/database-snapshots
+echo "# Truncating ${BACKUP_PATH} days back ${BACKUP_RETENTION}"
+OUTPUT=$($CWD/backup/dropbox-list-truncation-files.pl ${BACKUP_PATH} ${BACKUP_RETENTION})
+for x in $OUTPUT; do dropbox_uploader.sh delete ${BACKUP_PATH}/$x; done
+
+#############################################
+# cleanup
+#############################################
+echo "# Cleaning up..."
+sudo rm -rf /tmp/${MYSQL_BACKUP_NAME}.*
