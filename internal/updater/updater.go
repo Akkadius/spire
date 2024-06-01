e13b71269f6297fca612bd6c335354331bfd60d1
Akkadius <akkadius1@gmail.com>
2024-06-01 13:26:34 -0500

    Update updater.go

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -77,7 +77,7 @@ func (s *Updater) getAppVersion() (error, EnvResponse) {
 // CheckForUpdates checks for updates to the app
 func (s *Updater) CheckForUpdates(interactive bool) bool {
 	config, exists := s.serverconfig.GetIfExists()
-	if exists && config.Spire.DisableAutoUpdates {
+	if exists && config.Spire.DisableAutoUpdates && interactive {
 		s.logger.Info().Any("spire.disable_auto_updates", config.Spire.DisableAutoUpdates).Msg("Auto updates are disabled via config")
 		return false
 	}
